ec6cc5ce5e3c69136c02b86989a679103407846f
Emma Immink <Emma@wcw-stud-145-109-14-220.wireless.uva.nl>
2016-04-11 12:11:41 +0200

    Initial Commit

---
+++ demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by Emma Immink on 11-04-16.
+//  Copyright © 2016 Emma Immink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by Emma Immink on 11-04-16.
+//  Copyright © 2016 Emma Immink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
